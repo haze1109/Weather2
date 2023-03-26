f0e6a646be0c72959e51e7818fded7e116b25ab8
haze1109 <hazemorales11@gmail.com>
2023-03-26 20:12:23 +0800

    modification of index.html, main.js to main2.js

--- main.js
+++ main.js
@@ -75,19 +75,5 @@
       checkWeather(searchBox.value);
      }
     });
+    // const weatherIcon = document.querySelector('#weather-icon');
 
-
-
-
-
-
-
-
-// const searchInput = document.getElementById("searchInput");
-
-// searchInput.addEventListener("keypress", function(event) {
-//   if (event.keyCode === 13) {
-//     // Call the search function here
-//     console.log("Enter key pressed!");
-//   }
-// });
