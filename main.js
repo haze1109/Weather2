4af0d315f23ee2121e0cb378f854db23a92370de
Haze Morales <122412476+haze1109@users.noreply.github.com>
2023-03-28 17:16:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-    const apiKey = "34de024909b71804545c86726396e6d3";
+    const apiKey = "process.env.API_KEY";
     const apiUrl =
       "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
